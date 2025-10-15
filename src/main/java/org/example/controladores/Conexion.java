fadba7b780d01d2c43963b02e3bbeab451754c5a
eravebo <evelynraveb@gmail.com>
2025-10-15 17:52:29 -0500

    se crea proyecto

---
+++ src/main/java/org/example/controladores/Conexion.java
@@ -0,0 +1,24 @@
+package org.example.controladores;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    //Atributos de una Conexion
+    //url
+    private String url="jdbc:mysql://localhost:3307/usuarios_bd";
+    //usuario
+    private String usuario="root";
+    //contraseña
+    private String contraseña="";
+
+    public Connection conectar(){
+        return null;
+    }
+
+
+
+
+}
